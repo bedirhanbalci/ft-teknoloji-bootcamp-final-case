0aaee8fad273c9d0f4cd55766fc25f630cbebd4a
Bedirhan Balcı <bedirhanbalci@outlook.com>
2024-08-26 07:51:55 +0300

    Added Payment Client

---
+++ ticket-service/src/main/java/com/patika/ticketservice/client/payment/service/PaymentClientService.java
@@ -0,0 +1,19 @@
+package com.patika.ticketservice.client.payment.service;
+
+import com.patika.ticketservice.client.payment.PaymentClient;
+import com.patika.ticketservice.client.payment.service.dto.request.PaymentRequest;
+import com.patika.ticketservice.client.payment.service.dto.response.PaymentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentClientService {
+
+    private final PaymentClient paymentClient;
+
+    public PaymentResponse createPayment(PaymentRequest request) {
+        return paymentClient.createPayment(request).getData();
+    }
+
+}
---
+++ ticket-service/src/main/java/com/patika/ticketservice/client/payment/service/dto/response/enums/PaymentType.java
@@ -0,0 +1,9 @@
+package com.patika.ticketservice.client.payment.service.dto.response.enums;
+
+public enum PaymentType {
+
+    CREDIT_CARD,
+
+    EFT
+
+}
