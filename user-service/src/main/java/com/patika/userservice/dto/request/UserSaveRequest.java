b94e0e0fadf275b8b7255d32c4fe70a77d5c0bf1
Bedirhan Balcı <bedirhanbalci@outlook.com>
2024-08-26 08:53:38 +0300

    Added Auth Login Request, Auth Register Request, User Request, User Save Request dto

---
+++ user-service/src/main/java/com/patika/userservice/dto/request/UserSaveRequest.java
@@ -0,0 +1,20 @@
+package com.patika.userservice.dto.request;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class UserSaveRequest {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+}
