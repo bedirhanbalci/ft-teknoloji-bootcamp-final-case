f07640fbe67a7b560b02ed1620c48fb08a0eaf4f
Bedirhan Balcı <bedirhanbalci@outlook.com>
2024-08-26 07:26:00 +0300

    Added Gender, Travel Type, User Type, Voyage Status Enums

---
+++ ticket-service/src/main/java/com/patika/ticketservice/model/enums/Gender.java
@@ -0,0 +1,9 @@
+package com.patika.ticketservice.model.enums;
+
+public enum Gender {
+
+    MALE,
+
+    FEMALE
+
+}
---
+++ ticket-service/src/main/java/com/patika/ticketservice/model/enums/UserType.java
@@ -0,0 +1,9 @@
+package com.patika.ticketservice.model.enums;
+
+public enum UserType {
+
+    INDIVIDUAL,
+
+    CORPORATE
+
+}
