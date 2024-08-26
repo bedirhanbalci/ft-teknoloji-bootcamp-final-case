a94198558bf75dd6a91f4ff646e70d95820d72dc
Bedirhan Balcı <bedirhanbalci@outlook.com>
2024-08-26 09:26:48 +0300

    Added Notification Strategy, Email Notification Strategy, Sms Notification Strategy and Push Notification Strategy

---
+++ notification-service/src/main/java/com/patika/notificationservice/model/strategy/EmailNotificationStrategy.java
@@ -0,0 +1,23 @@
+package com.patika.notificationservice.model.strategy;
+
+import com.patika.notificationservice.model.Notification;
+import com.patika.notificationservice.model.enums.NotificationType;
+import com.patika.notificationservice.repository.NotificationRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class EmailNotificationStrategy implements NotificationStrategy {
+
+    private final NotificationRepository notificationRepository;
+
+    @Override
+    public void sendNotification(Notification notification) {
+        notification.setNotificationType(NotificationType.MAIL);
+        notificationRepository.save(notification);
+        log.info("Email notification recorded: " + notification.getMessage());
+    }
+}
