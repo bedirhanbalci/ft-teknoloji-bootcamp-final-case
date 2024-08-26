9fd0fcca0027aa02c8b44d3f5600424fc67195fc
Bedirhan Balcı <bedirhanbalci@outlook.com>
2024-08-26 08:13:26 +0300

    Added Payment Response, Generic Response and Generic Response Constant

---
+++ payment-service/src/main/java/com/patika/paymentservice/dto/response/PaymentResponse.java
@@ -0,0 +1,27 @@
+package com.patika.paymentservice.dto.response;
+
+import com.patika.paymentservice.model.enums.PaymentStatus;
+import com.patika.paymentservice.model.enums.PaymentType;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentResponse {
+
+    private Long bookingId;
+
+    private BigDecimal amount;
+
+    private PaymentType paymentType;
+
+    private LocalDateTime createdDateTime;
+
+    private PaymentStatus paymentStatus;
+
+}
