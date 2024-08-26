151d77bc65c482c3a8c701008e6103d1f96fce4d
Bedirhan Balcı <bedirhanbalci@outlook.com>
2024-08-26 07:52:22 +0300

    Added User Client

---
+++ ticket-service/src/main/java/com/patika/ticketservice/client/user/dto/response/UserResponse.java
@@ -0,0 +1,23 @@
+package com.patika.ticketservice.client.user.dto.response;
+
+import com.patika.ticketservice.client.user.dto.response.enums.UserType;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponse {
+
+    private Long userId;
+
+    private String email;
+
+    private String telephoneNumber;
+
+    private String receiverId;
+
+    private UserType userType;
+
+}
